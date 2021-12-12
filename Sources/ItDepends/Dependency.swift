1ef09873aa2d9de62dd03f76b9d18a2f64ea6c82
Ostap <ostap.hnatenko@gmail.com>
2021-12-12 20:36:45 +0200

    Add a property wrapper to observe changes in dependencies in a SwiftUI view

--- Sources/ItDepends/Dependency.swift
+++ Sources/ItDepends/Dependency.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
 
 protocol AnyDependency {
     var type: Model.Type { get }
@@ -23,7 +25,24 @@ public struct Dependency<Value: Model>: AnyDependency {
     var type: Model.Type { Value.self }
     
     mutating func resolve(with model: Model) {
-        value = model as! Value
+        value = (model as! Value)
+    }
+    
+    public init() {}
+}
+
+@propertyWrapper
+public struct ObservedDependency<Value: Model & ObservableObject>: AnyDependency, DynamicProperty {
+    private var observedObject: ObservedObject<Value>?
+    
+    public var wrappedValue: Value {
+        get { observedObject!.wrappedValue }
+    }
+    
+    var type: Model.Type { Value.self }
+    
+    mutating func resolve(with model: Model) {
+        observedObject = .init(wrappedValue: model as! Value)
     }
     
     public init() {}
